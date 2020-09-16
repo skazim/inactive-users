3a7dfc1f91a2d845406c7125f68d490761da3a48
skazim <kazimmuhammad10@gmail.com>
2020-09-17 01:35:54 +0500

    Updating time code

--- inactive.js
+++ inactive.js
@@ -2,7 +2,7 @@ const user = {
     name: "Qasim Asad",
     age: "25",
     city: "san francisco",
-    lastActivity: new Date().getTime()
+    lastActivity: new Date().getMinutes()
 }
 
 const inactiveFirstStage = () => {
@@ -19,6 +19,21 @@ const inactiveThirdStage = () => {
 
 
 // this is the main method that acts like a worker and runs every 1 minute
-setInterval(function(){
-    // please write your code here
+
+setInterval(function () {
+    var today = new Date();
+    var minute = today.getMinutes();
+    console.log(user.lastActivity, minute)
+    if(user.lastActivity + 2  <= minute){
+        console.log(user.lastActivity, minute,"First")
+        inactiveFirstStage();
+    }
+    else if(user.lastActivity + 4  <= minute){
+        console.log(user.lastActivity, minute,"Second")
+        inactiveSecondStage();
+    }
+    else if(user.lastActivity + 6  <= minute){
+        console.log(user.lastActivity, minute,"Third")
+        inactiveThirdStage();
+    }
 }, 1000);
